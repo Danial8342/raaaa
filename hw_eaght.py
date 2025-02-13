2b24c3a06a314a24862e6817b5ff134008408a06
AMIR <you@example.com>
2025-02-13 15:41:26 +0100

    debug

---
+++ hw_eaght.py
@@ -0,0 +1,45 @@
+from pywebio.input import input as pw_input
+from pywebio.output import put_text
+
+good_answers = 0
+
+
+ANSWER_ONE = 'Юпітер'
+ANSWER_TWO = '7'
+ANSWER_THREE = 'Париж'
+ANSWER_FOUR = 'Алюміній'
+ANSWER_FIVE = '7'
+
+
+question_one = pw_input(label='Яка планета є найбільшою у Сонячній системі')
+
+question_two = pw_input(label='Яка планета є найбільшою у Сонячній системі')
+
+question_three = pw_input(label='Яка планета є найбільшою у Сонячній системі')
+
+question_four = pw_input(label='Який метал є основним у виробництві алюмінієвої фольги?')
+
+question_five = pw_input(label='Яка планета є найбільшою у Сонячній системі')
+
+
+if question_one == ANSWER_ONE:
+    good_answers += 1
+
+
+if question_two == ANSWER_TWO:
+    good_answers += 1
+
+
+if question_three == ANSWER_THREE:
+    good_answers += 1
+
+
+if question_four == ANSWER_FOUR:
+    good_answers += 1
+
+
+if question_five == ANSWER_FIVE:
+    good_answers += 1
+
+
+put_text(f'Ви правильно відповіли на {good_answers} з 5 запитань.')
