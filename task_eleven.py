1864d2f0d7458a6de7aa7de1728df70cad2a90d0
AMIR <you@example.com>
2025-02-14 14:19:09 +0100

    change code

--- task_eleven.py
+++ task_eleven.py
@@ -5,5 +5,14 @@ def stricha_for_ten_long(slownyk):
             strichkas.append(strichka)
     print(strichkas)
 
+def is_konsisd(slownyk):
+    is_konsisd_hand = False
+    for word in slownyk:
+        if word  == "hand":
+            is_konsisd_hand = True
+            print(is_konsisd_hand)
 
-stricha_for_ten_long([" short", "muchlongerstring", "raaa", "adequate", "extremelylongstringexample"])
+
+stricha_for_ten_long([" short", "muchlongerstring", "hand", "adequate", "extremelylongstringexample"])
+is_konsisd([" short", "muchlongerstring", "hand", "adequate", "extremelylongstringexample"])
+
